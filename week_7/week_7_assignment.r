13a342c883a46c1723c00e9ce89105aee784a99b
Pavel Logacev <pavel.logacev@gmail.com>
2021-05-05 02:30:00 +0300

    Update.

--- week_7/week_7_assignment.r
+++ week_7/week_7_assignment.r
@@ -1,7 +1,8 @@
 
-# Week 7, 
+# Week 8
 
 # Assignment 1
+
 # We want to model the relationship between the time a horse will need to run 1000 meters as a function of that
 # horse's weight. (The underlying assumption is that all horses have roughly the same height, and that variation
 # in weight largely reflect muscles, and not fat.)
@@ -11,27 +12,46 @@
 # Step 1: Simulation parameters
 a = 150
 b = -1
-sigma_time = .1
+sigma_time = 2
 
-# Step 2: Generate horse weights
+# Step 2: Generate data
 n_horses = 100
-weight_kg = runif(n = n_horses, min = 50, max = 60)
-time_sec_hat = a + b*weight_kg
-time_sec_obs = rnorm(n_horses, mean = time_ms_hat, sd = 2)
 
-plot(weight_kg, time_sec_hat)
+# Generate horse weights in kg from a uniform distribution between 50 and 60. Use runif()
+weight_kg = 
+  
+# Calculate the average predicted time to finish a race, for each horse, according to the equation a + b * weight
+time_sec_hat = 
+  
+# Generate actual observations around the predicted mean by using rnorm()
+time_sec_obs = 
+  
+# Plot time_sec_hat by weight. Is this what it should look like?
 
-plot(weight_kg, time_sec_obs)
+  
+# Plot time_sec_obs by weight. Is this what it should look like?
 
-
-# Step 3: Create likelihood function
-likelihood_time <- function(time_sec, weight_kg, a, b) {
-  time_sec_hat <- a + b*weight_kg
-  lik <- rnorm(n_horses, mean = time_ms_hat, sd = 2)
-  prod(lik)
-}
-
-likelihood_time(time_sec, weight_kg, a, b)
   
 
+# Step 3: Create likelihood function
 
+# Complete this function to
+# (i) calculate the average *predicted* time to complete the race for each observation of weight
+# (ii) determine the log-likelihood of each *observed* time using dnorm(..., log=T), save the results to a vector
+# (iii) sum and return all log-likelihoods
+log_likelihood_time <- function(time_sec, weight_kg, a, b, sigma) {
+
+}
+
+# Test: This function should return a negative number. Does it? 
+log_likelihood_time(time_sec_obs, weight_kg, a, b, sigma_time)
+  
+
+# Step 4: Create a wrapper for the likelihood function
+log_likelihood_time_optim <- function(par) {
+  log_likelihood_time(time_sec_obs, weight_kg, par[["a"]], par[["b"]], par[["sigma"]])
+}
+
+# Call optim(). Does it recover the approximately correct parameters? If not, please double-check your code above. 
+res <- optim(c(a=100, b=-3, sigma=2), log_likelihood_time_optim, control = list(fnscale=-1))
+res
